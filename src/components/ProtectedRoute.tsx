a3527235fbd9f6c4dd7b8bdf21464f55f38201ed
aviram5 <60235058+aviram5@users.noreply.github.com>
2024-03-09 22:30:46 +0200

    Create ProtectedRoute.tsx

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+// const checkAuth = (): Promise<boolean> => {
+//   const user = false;
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(user);
+//     }, 500);
+//   });
+// };
+
+// const ProtectedRoute = () => {
+
+//     return()
+// }
+
+// export default ProtectedRoute
